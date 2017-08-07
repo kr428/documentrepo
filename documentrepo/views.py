570dcedf40ce77b3a5656a645e35864be9e555f3
Kristian Rink <rink@planconnect.de>
2017-08-07 20:54:09 +0200

    document api, first attempt

--- documentrepo/views.py
+++ documentrepo/views.py
@@ -1,6 +1,17 @@
 from documentrepo import main
 
+# default route
+# add something meaningful here.
+# 
 @main.route('/')
 @main.route('/index')
 def index():
     return "Hello, World!"
+
+@main.route('/docs')
+def list_documents():
+    return {}
+
+@main.route('/docs/<document>')
+def get_document(document):
+    return document
